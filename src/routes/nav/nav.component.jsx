6e7ad7edac3320edff0886d03d27c8bd0e02ea28
Grégory Marini <greg.marini@hotmail.fr>
2022-10-01 17:42:09 +0200

    creating an instance in firebase (installing in our project), create sign-in route

--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -14,6 +14,9 @@ const Navigation = () => {
                     <Link className="nav-link" to="/shop">
                         SHOP
                     </Link>
+                    <Link className="nav-link" to="/Sign-In">
+                        Sign-In
+                    </Link>
                 </div>
             </div>
             {/* *Output to the path */}
