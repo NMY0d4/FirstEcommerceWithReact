76ce38398ead94f5bf69f22c9d583013e3b28402
Grégory Marini <greg.marini@hotmail.fr>
2022-10-12 18:06:45 +0200

    adding cart context for toggle cartDropdown

--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -1,14 +1,17 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as GmwebLogo } from "../../assets/crown.svg";
+
 import { UserContext } from "../../contexts/user.context";
-
+import { ReactComponent as GmwebLogo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-
 import "./nav.styles.scss";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const Nav = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -29,7 +32,9 @@ const Nav = () => {
                             AUTHENTIFICATION
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             {/* *Output to the path */}
             <Outlet />
