00905be43c78949110cef7fb9a53d1a6042f24e3
Grégory Marini <greg.marini@hotmail.fr>
2022-10-18 22:23:36 +0200

    added link in home and category preview to go to one category

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,6 +1,7 @@
 import "./category-item.styles.scss";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CategoryItem = ({ title, imageUrl }) => {
     return (
@@ -9,10 +10,10 @@ const CategoryItem = ({ title, imageUrl }) => {
                 className="background-image"
                 style={{ backgroundImage: `url(${imageUrl})` }}
             />
-            <div className="category-body-container">
+            <Link className="category-body-container" to={`shop/${title}`}>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
-            </div>
+            </Link>
         </div>
     );
 };
