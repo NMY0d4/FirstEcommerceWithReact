f26b5b5d13f4c5007bd4ac85151d9ec0cfe73748
Grégory Marini <greg.marini@hotmail.fr>
2022-10-18 15:40:11 +0200

    created category preview, and refactoring routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Nav />}>
                 <Route index element={<Home />} />
-                <Route path="shop" element={<Shop />} />
+                <Route path="shop/*" element={<Shop />} />
                 <Route path="checkout" element={<Checkout />} />
                 <Route path="authentication" element={<Authentication />} />
             </Route>
