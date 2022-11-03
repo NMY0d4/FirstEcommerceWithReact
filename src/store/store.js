82b04d85cd246bdd2984cff72f0fbae9b29c82a6
Grégory Marini <greg.marini@hotmail.fr>
2022-11-03 22:48:30 +0100

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -9,11 +9,11 @@ const loggerMiddleware = (store) => (next) => (action) => {
     }
     console.log("type: ", action.type);
     console.log("payload: ", action.payload);
-    console.log("currentState: ", store.getState());
+    console.log("♥ currentState: ", store.getState());
 
     next(action);
 
-    console.log("next state: ", store.getState());
+    console.log("♥♥ next state: ", store.getState());
 };
 
 const middleWares = [loggerMiddleware];
