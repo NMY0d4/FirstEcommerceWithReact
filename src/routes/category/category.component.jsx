83e41617a916e1cd27522d080c38efe737000d44
Grégory Marini <greg.marini@hotmail.fr>
2022-11-08 19:25:13 +0100

    added redux 'thunk' and introduced spinner(for isLoaded condition with categories) , now only 'cart' persisted in localStorage

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,11 +4,16 @@ import { useState, useEffect, Fragment } from "react";
 // import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import {
+    selectCategoriesIsLoading,
+    selectCategoriesMap,
+} from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -18,12 +23,16 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toLocaleUpperCase()}</CategoryTitle>
-            <OneCatContainer>
-                {products &&
-                    products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
-                    ))}
-            </OneCatContainer>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <OneCatContainer>
+                    {products &&
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                </OneCatContainer>
+            )}
         </Fragment>
     );
 };
